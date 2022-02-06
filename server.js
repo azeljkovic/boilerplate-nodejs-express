a17a199d1d63772fc18a0fc34e470b12bd6fbd17
Aleksandar Zeljkovic <aleksandar.zeljkovic91@gmail.com>
2022-02-06 10:03:12 +0100

    added Timestamp Microservice API

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -18,15 +18,40 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// Timestamp Microservice API
+app.get("/api/:date?", function (req, res) {
+    let date = req.params.date;
+    let unix, utc, result;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+    // if date is a timestamp, convert to int
+    if(/^\d+$/.test(date)){
+        date = parseInt(date);
+    }
+
+    // if date is an empty param, send current time
+    if(date === undefined){
+        unix = Date.now();
+        utc = new Date().toUTCString();
+        result = {unix: unix,
+                  utc: utc};
+    }
+    // if date is invalid, return error
+    else if(new Date(date).toString() === "Invalid Date"){
+        result = { error : "Invalid Date" };
+    }
+    // if date is valid, convert
+    else{
+        unix = new Date(date).getTime();
+        utc = new Date(date).toUTCString();
+        result = {unix: unix,
+                  utc: utc};
+    }
+
+    res.json(result);
 });
 
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
